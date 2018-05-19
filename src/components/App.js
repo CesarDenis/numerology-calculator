d5696cecaa2f61365a3e0b71e0b7c71924c5af41
Cesar Denis <cesar@cesardenis.com>
2018-05-19 01:36:07 -0300

    feature: update app component

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import Form from './Form';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App__header">
+          <h1 className="App__title">
+            Welcome to the <span>Numerology</span> <strpng>Calculator</strpng>
+          </h1>
+        </header>
+
+        <Form />
+      </div>
+    );
+  }
+}
+
+export default App;
