10b2dc08a3c18f404c23dacd16614eca0b0d90a1
Cesar Denis <cesar@cesardenis.com>
2019-07-30 12:01:05 -0300

    feat: config styled component

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Home = () => (
-  <div>
-    <Title>Hello World!</Title>
-  </div>
-);
-
 const Title = styled.h1`
-  color: red;
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
 `;
 
+const Home = () => <Title>Hello World!</Title>;
+
 export default Home;
+
